92424f1daa3631737021dcd450855d5be70e63cf
Aman Vishwakarma <amanvishwakarma.baiml2025@huroorkee.ac.in>
2026-09-24 09:07:16 +0530

    Implement lonely integer function in LonelyInteger.py

---
+++ LonelyInteger.py
@@ -0,0 +1,19 @@
+import os
+
+def lonelyinteger(a):
+    result = 0
+    for num in a:
+        # XOR each number with the result
+        result ^= num 
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    
+    n = int(input().strip())
+    a = list(map(int, input().rstrip().split()))
+    
+    result = lonelyinteger(a)
+    
+    fptr.write(str(result) + '\n')
+    fptr.close()
